ee4eeec6be99c1ea9af8c08a17baa1d23dd6ea9f
Dai Luong <luong@DESKTOP-INLVFFC.localdomain>
2023-04-09 13:46:50 +0930

    Hi

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -20,22 +20,21 @@ int main() {
     for (int i = 0; i < n; i++) {
         string vehicle;
         int theID;
-        bool a;
 
         cout << "Please enter your vehicle and ID: ";
         cin >> vehicle;
         cin >> theID;
         if (vehicle == "Car") {
             ptr = new Car[theID];
-            a = parking.parkVehicle(ptr);
+            parking.parkVehicle(ptr);
             // sleep(3);
         } else if (vehicle == "Bus") {
             ptr = new Bus[theID];
-            a = parking.parkVehicle(ptr);
+            parking.parkVehicle(ptr);
             // sleep(3);
         } else if (vehicle == "Motorbike") {
             ptr = new Motorbike[theID];
-            a = parking.parkVehicle(ptr);
+            parking.parkVehicle(ptr);
             // sleep(3);
         }
     }
